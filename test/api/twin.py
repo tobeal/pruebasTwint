83b9dc416e0af2f738e47dc9fa764eb92acd6a93
Alberto <id00718167@usal.es>
2021-02-08 13:00:11 +0100

    [WIP] twin api

---
+++ test/api/twin.py
@@ -0,0 +1,41 @@
+#!flask/bin/python
+
+# Copyright 2020 AIR Institute
+# See LICENSE for details.
+
+import datetime
+from flask_restx import Resource
+
+from coaps import config
+from coaps.run import api
+from coaps.core import cache, limiter
+from coaps.api.pagination_arguments import pagination_arguments_parser
+from coaps.utils import handle400error, handle404error, handle500error
+
+from pruebasTwint.test.twitter_extractor import twitterExtractor
+
+twin_ns = api.namespace('twin', description='Obtain information from Twitter.')
+
+
+@twin_ns.route('/twin')
+class GetTweets(Resource):
+    @api.response(400, 'Invalid parameters')
+    @api.response(404, 'Data not found')
+    @api.response(500, 'Unhandled errors')
+    def get(self):
+        """
+        Returns a JSON object with the historic information of the weather.
+        Information is extracted from https://dev.meteostat.net/.
+        If pagination info is not provided, all information is served.
+        """
+
+        try:
+            json_data = twitterExtractor.test_twin_user_tweets(
+                'BarackObama', 'es', 20)
+        except:
+            return handle500error(athens_ns)
+
+        if not json_data:
+            return handle404error(athens_ns, 'No data was found')
+
+        return json_data
